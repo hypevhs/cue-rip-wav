f3bd7f10a041eeed0c94cd9dff28f0cb49fc99e0
Hype <hypershadsy@gmail.com>
2014-07-07 00:53:58 -0400

    Fix cross-directory shenanigans

--- cuerip.sh
+++ cuerip.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 
-ISOFILE=$(grep FILE "$1" | awk "{ print \$2 }" | tr -d \")
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PYTHONSCRIPT="${DIR}/cuerip.py"
+ISOFILENAME=$(grep FILE "$1" | awk "{ print \$2 }" | tr -d \")
+ISOFILE="$( dirname $1 )/${ISOFILENAME}"
 echo $ISOFILE
-grep "INDEX" "$1" | awk '{ print $3 }' | python "cuerip.py" "$ISOFILE"
+grep "INDEX" "$1" | awk '{ print $3 }' | python "${PYTHONSCRIPT}" "${ISOFILE}"
